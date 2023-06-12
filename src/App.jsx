b958ba3908a3a8a24e049aaf0a857363acc6ea89
Nya Haseley-Ayende <nhaseleyayende@salesforce.com>
2023-06-12 16:15:02 -0700

    finished step 5 - time for stretch features

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Dataset } from "./data/dataset"
 import "./App.css"
 import Header from "./components/Header/Header.jsx"
 import Chip from "./components/Chip/Chip.jsx" // no curly because exported default
+import NutritionLabel from "./components/NutritionalLabel/NutritionalLabel.jsx"
 import {useState} from "react"
 
 // don't move this!
@@ -25,6 +26,12 @@ export function App() {
   const { data, categories, restaurants } = Dataset.createDataSet()
   const[selectedCategory, selectCategory] = useState(null);
 
+  const[selectedRestaraunt, selectRestaraunt] = useState(null);
+  const[selectedMenuItem, selectMenuItem] = useState(null);
+  
+  const currentMenuItems = data.filter((item) => {
+    return item.food_category === selectedCategory && item.restaurant === selectedRestaraunt;
+  });
   return (
     <main className="App">
       {/* CATEGORIES COLUMN */}
@@ -65,7 +72,10 @@ export function App() {
               // const buttonClassName;
               return (
           <Chip 
-            label ={restaurant} key={restaurant} isActive={restaurant==="In-N-Out Burger"}
+            label ={restaurant} 
+            key={restaurant} 
+            isActive={selectedRestaraunt === restaurant} 
+            onClick={()=>selectRestaraunt(restaurant)}
             />)})
         }
           </div>
@@ -80,10 +90,32 @@ export function App() {
           <div className="MenuItemButtons menu-items">
             <h2 className="title">Menu Items</h2>
             {/* YOUR CODE HERE */}
+            {currentMenuItems.map((menuItem, i) => { // i??
+              // console.log("selected menu item ")
+              return (
+              <Chip 
+                label = {menuItem.item_name} 
+                key={`${menuItem.item_name}-${i}`} 
+                isActive={selectedMenuItem && selectedMenuItem.item_name === item.item_name} 
+                onClick={()=>selectMenuItem(menuItem)}
+
+              />)
+              })}
+
           </div>
 
           {/* NUTRITION FACTS */}
-          <div className="NutritionFacts nutrition-facts">{/* YOUR CODE HERE */}</div>
+          <div className="NutritionFacts nutrition-facts">
+            {/* YOUR CODE HERE */}
+            {selectedMenuItem ? <NutritionLabel 
+              // title={appInfo.title}
+              item={selectedMenuItem}
+            /> : null} 
+            {/* if there is an item, put it in */}
+            
+            
+            
+            </div>
         </div>
 
         <div className="data-sources">
